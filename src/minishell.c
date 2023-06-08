1b61887f24f8b4e664beac34e5ead970fcc2a627
Jonah Köllner <koellnjo@gmail.com>
2023-06-08 21:04:12 +0200

    cleaned into folder and adjusted makefile

--- src/minishell.c
+++ src/minishell.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jonahkollner <jonahkollner@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 12:20:39 by jonahkollne       #+#    #+#             */
-/*   Updated: 2023/06/07 12:50:23 by jonahkollne      ###   ########.fr       */
+/*   Updated: 2023/06/08 21:02:46 by jonahkollne      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	print_history(t_history *history)
 	t_history *iter;
 
 	iter = history;
-	printf("print history:\n");
+	printf("print history:\n\n");
 	while (iter != NULL)
 	{
 		printf("%s\n", iter->command);
@@ -40,16 +40,18 @@ int	main(void)
 
 	while (1)
 	{
-		current = malloc(sizeof(t_history));
 		command = malloc(sizeof(char) * 1000);
 		printf("\nminishell>:$ ");
 		fgets(command, 1000, stdin);
 		printf("\n exec %s\n", command);
+		if (strncmp(command, "history", 7) == 0)
+			print_history(current);
+		if (strncmp(command, "exit", 4) == 0)
+			break ;
+		current = malloc(sizeof(t_history));
 		current->command = command;
 		current->previous = previous;
 		previous = current;
-		if (strncmp(current->command, "history", 8) == 0)
-			print_history(current);
 	}
 	return (1);
 }
