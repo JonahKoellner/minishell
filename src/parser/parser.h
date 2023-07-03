b56ffc7cf7fb6398a6b76f63613f3ff515852ba7
RowMax03 <51720224+RowMax03@users.noreply.github.com>
2023-07-03 15:22:19 +0200

    Lexer Start Needs renaming

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,18 +6,27 @@
 /*   By: mreidenb <mreidenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 11:51:09 by mreidenb          #+#    #+#             */
-/*   Updated: 2023/06/27 12:20:02 by mreidenb         ###   ########.fr       */
+/*   Updated: 2023/06/27 16:07:22 by mreidenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
+# include <stdlib.h>
+# include "../lib/42-libft/libft.h"
+
 typedef enum TokenType
 {
 	TOKEN_WORD,
 	TOKEN_VARIABLE,
 	TOKEN_COMMAND_NAME,
+	TOKEN_REDIRECT,
+	TOKEN_GREAT,
+	TOKEN_LESS,
+	TOKEN_GREAT_GREAT,
+	TOKEN_LESS_LESS,
+	TOKEN_PIPE,
 	TOKEN_LEFT_PAREN,
 	TOKEN_RIGHT_PAREN
 }	t_TokenType;
--- src/parser/parser_main.c
+++ src/parser/parser_main.c
@@ -6,13 +6,73 @@
 /*   By: mreidenb <mreidenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 12:01:12 by mreidenb          #+#    #+#             */
-/*   Updated: 2023/06/27 12:34:00 by mreidenb         ###   ########.fr       */
+/*   Updated: 2023/06/27 16:39:52 by mreidenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
+static char	*fillstr(const char *str, int z, int fin)
+{
+	char	*fill;
+	int		i;
+
+	i = 0;
+	if (!*str)
+		return (NULL);
+	fill = (char *)malloc((fin - z + 1) * sizeof(char));
+	if (!fill)
+		return (NULL);
+	while (z < fin)
+		fill[i++] = str[z++];
+	fill[i] = 0;
+	return (fill);
+}
+
+t_Token	redirect_decide(t_Token token, int *i)
+{
+	if (token.lexeme[0] == '>' && token.lexeme[1] != '>')
+		token.type = TOKEN_GREAT;
+	else if (token.lexeme[0] == '>' && token.lexeme[1] == '>')
+		token.type = TOKEN_GREAT_GREAT;
+	else if (token.lexeme[0] == '<' && token.lexeme[1] != '<')
+		token.type = TOKEN_LESS;
+	else if (token.lexeme[0] == '<' && token.lexeme[1] == '<')
+		token.type = TOKEN_LESS_LESS;
+	else if (token.lexeme[0] == '|' && token.lexeme[1] != '|')
+		token.type = TOKEN_PIPE;
+	if (token.type == TOKEN_GREAT_GREAT || TOKEN_LESS_LESS)
+		*i++;
+	free(token.lexeme);
+	return (token);
+}
+
+//gets the next token, after complete command call with command = NULL
 t_Token	get_next_token(char *command)
 {
+	t_Token		token;
+	static int	i = 0;
+	int			j;
 
+	if (!command)
+		i = 0;
+	while (command[i] == ' ', command[i] == '\t', command[i] == '\n')
+		i++;
+	if (ft_isalnum(command[i]))
+	{
+		j = i;
+		while (ft_isalnum(command[i]))
+			i++;
+		token.lexeme = fillstr(command, j, i);
+		token.type = TOKEN_WORD;
+		return (token);
+	}
+	if (command[i] == '>' || command[i] != '<' || command[i] != '|')
+	{
+		token.type = TOKEN_REDIRECT;
+		token.lexeme = fillstr(command, i, i + 1);
+		i++;
+		return (redirect_decide(token, &i));
+	}
+	return (token);
 }
