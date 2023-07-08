96f2405f6288ff7df2beddd38513fed18586e0e5
Jonah Köllner <koellnjo@gmail.com>
2023-07-08 10:11:45 +0200

    merge

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jonahkollner <jonahkollner@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 12:35:46 by jonahkollne       #+#    #+#             */
-/*   Updated: 2023/07/05 18:52:18 by jonahkollne      ###   ########.fr       */
+/*   Updated: 2023/07/08 09:57:43 by jonahkollne      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,19 @@
 # include <signal.h>
 # include <sys/types.h>
 
-int	new_line(void);
+# include "lib/42-libft/libft.h"
+
+//#--------- Structs ----------#//
+typedef struct sys_val
+{
+	char	*dir;
+}	t_sys_val;
+
+//#--------- visual_prompt ----------#//
+int		new_line(void);
+
+//#--------- signal_handler ----------#//
+void	sig_ctrl_c(int signo, siginfo_t *client_info, void *param);
+void	sig_ctrl_d(int signo, siginfo_t *client_info, void *param);
 
 #endif
