62bbd840dca66d74d0de0a59e9bc55fdd978505f
RowMax03 <51720224+RowMax03@users.noreply.github.com>
2023-07-10 15:45:54 +0200

    head fix

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mreidenb <mreidenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 12:35:46 by jonahkollne       #+#    #+#             */
-/*   Updated: 2023/07/10 15:34:01 by mreidenb         ###   ########.fr       */
+/*   Updated: 2023/07/10 15:45:42 by mreidenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,21 +31,6 @@
 # define STDOUT 1
 # define STDIN 0
 # define STDERR 2
-
-# include "lib/42-libft/libft.h"
-
-//#--------- Structs ----------#//
-typedef struct sys_val
-{
-	char	*dir;
-}	t_sys_val;
-
-//#--------- visual_prompt ----------#//
-int		new_line(void);
-
-//#--------- signal_handler ----------#//
-void	sig_ctrl_c(int signo, siginfo_t *client_info, void *param);
-void	sig_ctrl_d(int signo, siginfo_t *client_info, void *param);
 
 # include "lib/42-libft/libft.h"
 
