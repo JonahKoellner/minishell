99631ded1cfb0a571607da5e83186b3f7112299f
Jonah Kollner <jkollner@2-H-7.42heilbronn.de>
2023-08-18 15:46:02 +0200

    sampled executer

--- src/utils/executer.c
+++ src/utils/executer.c
@@ -6,13 +6,20 @@
 /*   By: jkollner <jkollner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 11:04:52 by jkollner          #+#    #+#             */
-/*   Updated: 2023/08/18 11:05:08 by jkollner         ###   ########.fr       */
+/*   Updated: 2023/08/18 14:01:17 by jkollner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	executer()
+int	command_name(t_Command command, char **envp)
 {
+	return (0);
+}
 
+int	executer(t_Command command, char **envp)
+{
+	if (command.type.type == TOKEN_COMMAND_NAME)
+		command_name(command, envp);
+	return (0);
 }
