dd66ce0bad7f7322ae5526c626fe2dc54b6888fc
Jonah Kollner <jkollner@2-H-5.42heilbronn.de>
2023-08-30 10:29:36 +0200

    removed error freed pointer was not allocated. Still no leaks

--- src/utils/enviroment.c
+++ src/utils/enviroment.c
@@ -6,7 +6,7 @@
 /*   By: jkollner <jkollner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 16:07:37 by jkollner          #+#    #+#             */
-/*   Updated: 2023/08/29 15:56:34 by jkollner         ###   ########.fr       */
+/*   Updated: 2023/08/30 10:26:33 by jkollner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ char	**enviroment(char	**envp)
 
 	if (envp)
 	{
-		if (env)
-			ft_vecfree(env);
+		//if (env)
+		//	ft_vecfree(env);
 		env = ft_vecdup(envp);
 		ft_vecfree(envp);
 	}
@@ -29,9 +29,6 @@ char	**enviroment(char	**envp)
 
 /// Add variable to the enviroment or update existing variable
 /// Var contains the "equation" for the var (i.e: "HOME=/Users/")
-
-/// Improvement: Make the parameter big enough for multiple assignments,
-/// so its only one realloc for multiple add
 int	add_environ(char	*var)
 {
 	if (!ft_vecreplace(var, enviroment(NULL)))
